8d4dd5bb87cdacb6bda795920731d04e42b09cf1
Sherly Mauren <IT110@kbinsure.co.id>
2021-10-30 17:41:51 +0700

    adjust product

--- app/Views/product.php
+++ app/Views/product.php
@@ -14,12 +14,14 @@
 <div class="container d-flex h-100">
     <div class="row align-self-center justify-content-around w-100">
         <?php foreach ($products as $key=>$product) { ?>
-            <div class="card" style="width: 18rem;">
+            <div class="card" style="width: 15rem;">
                 <img class="card-img-top" src="<?php echo 'http://localhost:8080/apime/public/assets/uploads/' . $product['product_image']; ?>" alt="Card image cap">
                 <div class="card-body">
                     <h5 class="card-title text-center"><?= $product['product_name']; ?></h5>
                     <h5 class="card-title text-center">Rp <b><?= number_format($product['price']); ?></b></h5>
                     <p class="card-text"><?= $product['description']; ?></p>
+                </div>
+                <div class="card-footer">
                     <div class="d-flex justify-content-center">
                         <a href="#" class="btn btn-primary btn-info">Beli</a>
                     </div>
@@ -28,8 +30,8 @@
         <?php } ?>
     </div>
 </div>
-<!--<div class="row">-->
-<!--    <div class="col-md-12 d-flex justify-content-center" style="margin-top: auto !important;">-->
-
-<!--    </div>-->
-<!--</div>-->
+<footer class="footer">
+    <div class="container">
+        <span class="text-muted">2021 PT Majoo Teknologi Indonesia</span>
+    </div>
+</footer>
